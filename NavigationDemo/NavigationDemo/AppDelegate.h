38160308d03fab35ee1c5fa5f47e9a46cce7a764
daiyunpeng <daiyunpeng@daiyunpengdeMac-mini.local>
2017-06-01 13:53:17 +0800

    初始化工程

---
+++ NavigationDemo/NavigationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationDemo
+//
+//  Created by daiyunpeng on 2017/6/1.
+//  Copyright © 2017年 Elegant Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavigationDemo/NavigationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationDemo
+//
+//  Created by daiyunpeng on 2017/6/1.
+//  Copyright © 2017年 Elegant Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
